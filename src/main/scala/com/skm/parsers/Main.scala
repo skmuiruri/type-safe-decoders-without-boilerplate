3f3e14018207f4dd7b9908837e036347803a362f
Samson Kamau Muiruri <kamau.muiruri@hivemindtechnologies.com>
2023-11-21 14:31:29 +0100

    bugfix

--- src/main/scala/com/skm/parsers/Main.scala
+++ src/main/scala/com/skm/parsers/Main.scala
@@ -5,18 +5,26 @@ import zio.{ Chunk, ZIOAppDefault }
 
 object Main extends ZIOAppDefault {
 
-  val params: Map[String, List[String]] = Map(
-    "title"   -> List("Good Omens"),
-    "authors" -> List("Neil Gaiman", "Terry Pratchett")
-//    "Moby-Dick" -> List("Herman Melville"),
-//    "For Whom the bell toils" -> Chunk("Oscar wilde"),
-//    "Good Omens" -> Chunk("Neil Gaiman", "Terry Pratchett"),
-//    "Divine Comedy" -> Chunk("Dante Alighieri")
+  val paramChunks: Map[String, Chunk[String]] = Map(
+    "title"   -> Chunk("Good Omens"),
+    "authors" -> Chunk("Neil Gaiman", "Terry Pratchett"),
   )
 
-  def run =
-    usingdynamicvalue.QueryParamsParser
+  val paramList: Map[String, List[String]] = Map(
+    "title" -> List("Good Omens"),
+    "authors" -> List("Neil Gaiman", "Terry Pratchett"),
+  )
+
+  def run = {
+
+//    usingdynamicvalue.QueryParamsParser
+//      .decode[Book]
+//      .debug("results")
+//      .provide(usingdynamicvalue.QueryParamsParser.make(paramList))
+
+    usingpatternmatching.QueryParamsParser
       .decode[Book]
-      .debug("results: ")
-      .provide(usingdynamicvalue.QueryParamsParser.make(params))
+      .debug("results")
+      .provide(usingpatternmatching.QueryParamsParser.make(paramChunks))
+  }
 }
